f660b1f8855a2a826bd3700d39c123a1fa3de420
Aleksandar Yalnazov <yalnazov@gmail.com>
2014-01-21 18:53:18 +0200

    Remove commented code.

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -29,31 +29,6 @@
     }
     return self;
 }
-//- (void)setSelected:(BOOL)selected {
-//    _selected = selected;
-//    // Update view as state changed
-//    [self setNeedsDisplay];
-//}
-//- (void)drawRect:(CGRect)rect {
-//    
-//    UIBezierPath *bezierPath;
-//    
-//    // Draw top line
-//    bezierPath = [UIBezierPath bezierPath];
-//    [bezierPath moveToPoint:CGPointMake(0.0, 0.0)];
-//    [bezierPath addLineToPoint:CGPointMake(CGRectGetWidth(rect), 0.0)];
-//    [[UIColor colorWithWhite:197.0/255.0 alpha:0.75] setStroke];
-//    [bezierPath setLineWidth:1.0];
-//    [bezierPath stroke];
-//    
-//    // Draw bottom line
-//    bezierPath = [UIBezierPath bezierPath];
-//    [bezierPath moveToPoint:CGPointMake(0.0, CGRectGetHeight(rect))];
-//    [bezierPath addLineToPoint:CGPointMake(CGRectGetWidth(rect), CGRectGetHeight(rect))];
-//    [[UIColor colorWithWhite:197.0/255.0 alpha:0.75] setStroke];
-//    [bezierPath setLineWidth:1.0];
-//    [bezierPath stroke];
-//}
 @end
 
 #pragma mark - ViewPagerController
